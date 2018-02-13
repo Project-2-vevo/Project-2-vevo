bb6dbc24ae0dd7d90bb1435e02ad1ca064c99d19
AlbertoSP <ALBERTOSANCHEZHTML@GMAIL.COM>
2018-02-13 12:46:56 +0100

    cambios

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,50 @@ const User = require("../models/User");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  console.log('hola')
   res.send('respond with a resource');
 });
 
 /* CRUD -> READ DETAIL */
-router.get('/detail/:id', (req,res) => {
+router.get('/detail/:id', (req, res) => {
   const userId = req.params.id;
   console.log(userId)
 
   User.findById(userId, (err, user) => {
-    if (err) { return next(err); }
-    res.render('detail', {user: user});
+    if (err) {
+      return next(err);
+    }
+    res.render('detail', {
+      user: user
+    });
   })
-}); 
+});
+
+router.get('/detail/:id/edit', (req, res) => {
+  const userId = req.params.id;
+  
+  User.findById(userId, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    res.render('editUser')
+  })
+})
+
+router.post('/detail/:id/edit', (req, res) => {
+  const userId = req.params.id;
+  const username = req.body.username;
+  console.log(req.body)
+  const updates = {username:req.body.username };
+  
+  User.findByIdAndUpdate(userId, updates, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    res.render("editUser")
+  })
+})
+
+
 module.exports = router;
